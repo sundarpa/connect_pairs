87c46f3090dc217bdf68b96dd778ac77b58d390c
Vidya2000 <vidyagms9634@gmail.com>
2023-10-25 18:34:02 +0530

    Added output.csv to write the enable Column as per the conditions.

--- Enable_column.py
+++ Enable_column.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 # Define the paths to the CSV files
 conditions_csv_file = r"C:\Users\Dell\Downloads\Conditions.csv"
-input_csv_file = r"C:\Users\Dell\Downloads\INPUT.csv"
+input_csv_file = r"C:\Users\Dell\Downloads\Input.csv"
+output_csv_file = r"C:\Users\Dell\Downloads\Output.csv"
 
 # Load the conditions and input CSV data into DataFrames
 conditions_df = pd.read_csv(conditions_csv_file, delimiter=',')
@@ -10,7 +11,7 @@ input_df = pd.read_csv(input_csv_file, delimiter=',')
 
 # Define a function to update 'Enable' column based on conditions
 def update_enable_column(row):
-    enable_value = ''
+    enable_value = None  # Initialize with None
 
     for column in row.index[1:]:  # Exclude the 'Enable' column
         if row[column] == 'Y':
@@ -18,18 +19,18 @@ def update_enable_column(row):
             break
         elif row[column] == 'N':
             enable_value = 'N'
-        else:
-            enable_value = 'NIL'
+
+    if enable_value is None:
+        enable_value = 'NIL'  # Set to 'NIL' if no 'Y' or 'N' is found
 
     # Update 'Enable' column
     row['Enable'] = enable_value
-
     return row
 
 # Apply the 'Enable' function to the input DataFrame
 input_df = input_df.apply(update_enable_column, axis=1)
 
-# Define a function to update 'S1', 'S2', 'S3', 'S4' columns based on the conditions
+# Define a function to update S columns based on the conditions
 def update_s_columns(row):
     for column in input_df.columns[1:]:  # Exclude the 'Enable' column
         if column.startswith('S'):
@@ -49,7 +50,7 @@ def update_s_columns(row):
 input_df = input_df.apply(update_s_columns, axis=1)
 
 # Write the updated columns back to the original input CSV file
-input_df.to_csv(input_csv_file, index=False, sep=',', mode='w')
+input_df.to_csv(output_csv_file, index=False, sep=',', mode='w')
 
 # Print the updated DataFrame
 print(input_df)

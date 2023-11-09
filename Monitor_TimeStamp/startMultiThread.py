4161d4c1a54c220f6cbb7854cbd89c0f15126112
sundaravarman <rsundaravarman@gmail.com>
2023-11-10 00:03:15 +0530

    enhanced to output 5 forloop df and combine the final result

--- Monitor_TimeStamp/startMultiThread.py
+++ Monitor_TimeStamp/startMultiThread.py
@@ -1,20 +1,27 @@
 import threading
 import time
 import random
+import pandas as pd
 
-def random_hello(username):
-    wait_time = random.uniform(1, 5)
-    print("Waiting for {} seconds before saying hello to {}...".format(wait_time, username))
-    time.sleep(wait_time)
-    print("Hello, {}!".format(username))
+def generate_random_df(username):
+    headers = ['A', 'B', 'C', 'D']
+    data = {'A': random.randint(1, 10),
+            'B': random.randint(1, 10),
+            'C': random.randint(1, 10),
+            'D': random.randint(1, 10)}
+
+    df = pd.DataFrame([data], columns=headers)
+    print("{} generated DataFrame:\n{}".format(username, df))
+    return df
 
 # Create a list to store thread instances
 threads = []
 
 # Create and start 5 threads, passing a unique username to each thread
+dfs = []
 for i in range(5):
     username = "User{}".format(i + 1)
-    thread = threading.Thread(target=random_hello, args=(username,))
+    thread = threading.Thread(target=lambda: dfs.append(generate_random_df(username)))
     threads.append(thread)
     thread.start()
 
@@ -22,4 +29,6 @@ for i in range(5):
 for thread in threads:
     thread.join()
 
-print("All threads have finished.")
+# Combine all DataFrames into one
+result_df = pd.concat(dfs, ignore_index=True)
+print("\nCombined DataFrame:\n", result_df)
